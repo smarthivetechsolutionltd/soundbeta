584732e293c1827e84a035c3bf064c7a58d590e2
AOAT studios <57410469+aoatstudios@users.noreply.github.com>
2023-05-01 13:31:33 +0100

    update

--- Screen/Pages/HomePage/Styles.js
+++ Screen/Pages/HomePage/Styles.js
@@ -19,7 +19,7 @@ const styles = StyleSheet.create({
         width: '100%',
         backgroundColor: Primary,
         // padding: 20,
-        paddingTop: statusBarHeight,
+        // paddingTop: statusBarHeight,
     },
 
     homeinnerContainer: {
